19019804eefe7c53839dcda12f625bd0d95589a8
NatC02 <NatanCeballos02@gmail.com>
2021-03-05 23:23:01 -0500

    adding files for pr

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+let f = function () {
+  console.log("Hello There");
+}
+
+f();
+
+//the above function cannot be assigned another value, as in f cannot be assgined a number value and such
+
+let message = function simpleConsoleMessage(){
+  console.log("just another message");
+}
+
+vibrateBtn = document.createElement('button');
+vibrateBtn.setAttribute("id", "btn");
+
+//check for the different versions of the vibrate api (comes with HTML5)
+navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+
+if (navigator.vibrate) {
+  // checks if api is supported for browser
+  vibrateBtn.addEventListener('click', function(ev) {
+  console.log('Vibration has worked!');
+  navigator.vibrate(1000);
+      });
+  }
+
+  (function(ev) {
+  console.log("you can try making your own theme, also this api will drain your battery! Its too much power!")
+  })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* This was an initial test of understanding location
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition((position) => {
@@ -9,6 +60,12 @@ if (navigator.geolocation) {
 }
 
 */
+
+
+
+/*
+
+
 
 function initMap() {
 
@@ -103,6 +160,8 @@ function initMap() {
     }
   }
 }
+
+*/
 
 /*
 
